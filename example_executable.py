f76bf6b4a7a1091bcd1d1e51b2cd411ea33bdc9b
callie-jones <callie.jones@colorado.edu>
2018-02-19 13:30:26 -0700

    reading live data into mongo from temperature sensor and outputting to ui

--- example_executable.py
+++ example_executable.py
@@ -1,1 +1,6 @@
-print("2014/10/30 07:00:36 Temperature 73.96F 23.31C")
+import os, subprocess
+curr = os.getcwd()
+dirs = os.chdir(curr+'../bits-weather-dashboard/sensor_drivers/thermometer/')
+p = subprocess.Popen(['./pcsensor'],cwd=dirs)
+p.wait()
+#print("2014/10/30 07:00:36 Temperature 73.96F 23.31C")
