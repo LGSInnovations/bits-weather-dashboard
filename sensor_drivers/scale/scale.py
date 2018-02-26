6f4dbdce117f3d51b6271ef430c4b06920bb9088
Jack Dinkel <johnpauldinkel@gmail.com>
2018-02-25 19:52:04 -0700

    Refined scale driver

--- sensor_drivers/scale/scale.py
+++ sensor_drivers/scale/scale.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python
-import os, time
+import time
 import usb.core
 import usb.util
-import pygtk
-pygtk.require('2.0')
-import gtk
 from sys import exit
-import math
 
 # DYMO 100 lbs
 VENDOR_ID = 0x0922
@@ -20,11 +16,9 @@ def main():
     try:
         # Was device found?
         if dev is None:
-            print "device not found"
+            print("Device not found")
             exit()
         else:
-            print "device found"
-
             # Deactivate other drivers
             interface = 0
             if dev.is_kernel_driver_active(interface) is True:
@@ -37,54 +31,55 @@ def main():
             listen()
 
     except KeyboardInterrupt as e: 
-        exit();
+        exit()
 
 
 def grab():
     try:
-        # first endpoint
+        # Set first endpoint
         endpoint = dev[0][(0,0)][0]
 
-        # read a data packet
+        # Read a data packet. Try 10 times before failure.
         attempts = 10
         data = None
         while data is None and attempts > 0:
             try:
                 data = dev.read(endpoint.bEndpointAddress,
-                                   endpoint.wMaxPacketSize)
+                                endpoint.wMaxPacketSize)
             except usb.core.USBError as e:
                 data = None
                 if e.args == ('Operation timed out',):
                     attempts -= 1
-                    #print "timed out... trying again"
+                    #print("timed out... trying again")
                     if attempts < 1:
                         print('Timed out')
                     continue
         return data
+
     except usb.core.USBError as e:
-        print("USBError: " + str(e.args))
+        print("USBError:", str(e.args))
+        exit()
     except IndexError as e:
-        print("IndexError: " + str(e.args))
+        print("IndexError:", str(e.args))
+        exit()
 
 
 def listen():
     DATA_MODE_KG  = 3
     DATA_MODE_LBS = 12
 
-    while True:
-        time.sleep(.5)
+    data = grab()
 
-        weight = 0
-
-        data = grab()
-        if data != None:
-            raw_weight = (data[4] + (data[5] * 256)) / 10.0
-            mode = "lbs" if (data[2] == DATA_MODE_LBS) else "kg"
-            
-	    print("%s, %.1f %s" % (time.time(), raw_weight, mode))
-	else:
-	    print("Error grabbing data")
-
+    # Interpret data
+    if data != None:
+        raw_weight = (data[4] + (data[5] * 256)) / 10.0
+        if data[2] != DATA_MODE_LBS:
+            print("Data is not in lbs")
+            exit()
+        
+        print("date:%d reading:%.1f lbs" % (time.time(), raw_weight))
+    else:
+        print("Error grabbing data")
 
 
 
